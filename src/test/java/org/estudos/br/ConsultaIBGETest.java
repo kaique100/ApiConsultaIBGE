8475ae415a94ae93030bd4a0cdf2c58d05ad4d94
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-26 17:14:44 -0300

    subindo o projeto com dois testes feitos

---
+++ src/test/java/org/estudos/br/ConsultaIBGETest.java
@@ -0,0 +1,93 @@
+package org.estudos.br;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConsultaIBGETest {
+    private static final String ESTADOS_API_URL = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/";
+    private static final String DISTRITO_API_URL = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos/";
+
+    private static final String REGIOES_API_URL = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes/";
+    private static final String ESTADO = "/estado";
+
+    @Test
+    @DisplayName("Teste para consulta única de um estado")
+    public void testConsultarEstado() throws IOException {
+        // Arrange
+        String uf = "SP"; // Define o estado a ser consultado
+
+        // Act
+        String resposta = ConsultaIBGE.consultarEstado(uf); // Chama o método a ser testado
+
+        // Assert
+        // Verifica se a resposta não está vazia
+        assert !resposta.isEmpty();
+
+        // Verifica se o status code é 200 (OK)
+        HttpURLConnection connection = (HttpURLConnection) new URL(ESTADOS_API_URL + uf).openConnection();
+        int statusCode = connection.getResponseCode();
+        assertEquals(200, statusCode, "O status code da resposta da API deve ser 200 (OK)");
+    }
+
+    @Test
+    @DisplayName("Teste para consulta única de um distrito")
+    public void testConsultarDistrito() throws IOException {
+        // Arrange
+        int distrito = 160030312; // Define o estado a ser consultado
+
+        // Act
+        String resposta = ConsultaIBGE.consultarDistrito(distrito); // Chama o método a ser testado
+
+        // Assert
+        // Verifica se a resposta não está vazia
+        assert !resposta.isEmpty();
+
+        // Verifica se o status code é 200 (OK)
+        HttpURLConnection connection = (HttpURLConnection) new URL(DISTRITO_API_URL + distrito).openConnection();
+        int statusCode = connection.getResponseCode();
+        assertEquals(200, statusCode, "O status code da resposta da API deve ser 200 (OK)");
+    }
+
+    @Test
+    @DisplayName("Teste para consulta única de uma região")
+    public void testConsultarRegiao() throws IOException {
+        // Arrange
+        int regiao = 3; // Define o estado a ser consultado
+
+        // Act
+        String resposta = ConsultaIBGE.consultarRegiao(regiao); // Chama o método a ser testado
+
+        // Assert
+        // Verifica se a resposta não está vazia
+        assert !resposta.isEmpty();
+
+        // Verifica se o status code é 200 (OK)
+        HttpURLConnection connection = (HttpURLConnection) new URL(REGIOES_API_URL + regiao).openConnection();
+        int statusCode = connection.getResponseCode();
+        assertEquals(200, statusCode, "O status code da resposta da API deve ser 200 (OK)");
+    }
+
+    @Test
+    @DisplayName("Teste para consulta única de uma unica regiao de varios estados")
+    public void testConsultarRegiaoComEstado() throws IOException {
+        // Arrange
+        int regiaoDoEstado = 3; // Define o estado a ser consultado
+
+        // Act
+        String resposta = ConsultaIBGE.consultarRegiao(regiaoDoEstado); // Chama o método a ser testado
+
+        // Assert
+        // Verifica se a resposta não está vazia
+        assert !resposta.isEmpty();
+
+        // Verifica se o status code é 200 (OK)
+        HttpURLConnection connection = (HttpURLConnection) new URL(REGIOES_API_URL + regiaoDoEstado + ESTADO).openConnection();
+        int statusCode = connection.getResponseCode();
+        assertEquals(200, statusCode, "O status code da resposta da API deve ser 200 (OK)");
+    }
+}
